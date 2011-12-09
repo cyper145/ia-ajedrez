4c3f2552c4d36589c9bc7be0345e77ad3c01d3cb
buster092 <buster092@gmail.com>
2011-12-09 15:17:20 +0000

    add nodos a arbol y busca jugadas

--- src/example/Test.java
+++ src/example/Test.java
@@ -7,12 +7,16 @@ package example;
 */
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Random;
+
+import util.Node;
+import util.Tree;
 public class Test {
 	Board b;
 	
 	public static void main(String[] args) {
-
+		
 		Random r=new Random();
 		int[][] board = new int[8][8];
 		Board b=new Board();
@@ -28,8 +32,8 @@ public class Test {
 			String turn=input.readLine();
 			//System.out.println(turn);
 			b.fromArray(board);
-			if (turn.equals("N")) b.setTurn(b.TURNBLACK);
-			else b.setTurn(b.TURNWHITE);
+//			if (turn.equals("N")) b.setTurn(b.TURNBLACK);
+//			else b.setTurn(b.TURNWHITE);
 			b.setShortCastle(b.TURNBLACK,false);
 			b.setLongCastle(b.TURNBLACK,false);
 			b.setShortCastle(b.TURNWHITE,false);
@@ -50,9 +54,10 @@ public class Test {
 				st=input.readLine();
 				
 			}
-			//System.out.println(b);
+			System.out.println(b);
 	
 		} catch (Exception e) {}
+		/*
 		System.out.println(b);
 		Move[] moves=b.getValidMoves();
 		System.out.println("Stalemate:"+b.isStalemate());
@@ -60,8 +65,68 @@ public class Test {
 		for (Move m:moves) {
 			System.out.println(m);
 		}
+		*/
+		
+		Tree t = new Tree();
+		Node root =  new Node (b);
+		t.setRootElement(root);
+		makeTree(root, 0);
+		/*Move[] moves=b.getValidMoves();
+		for (Move m:moves) {
+			Board b_child = b.clone();
+			b_child.makeMove(m);
+			Node n =  new Node (b_child);
+			 root.addChild(n);
+		}*/
+		//System.out.println(root.toString());
+		
 	}
 	
-	
+	private static int makeTree(Node n, int vuelta){
+		vuelta++;
+		Board b = (Board) n.data;
+		Move[] moves=b.getValidMoves();
+		
+		/*
+		if(moves.length>0){
+			Board b_child = b.clone();
+			b_child.makeMove(moves[0]);
+			System.out.println("Movimiento: "+moves[0].toString());
+			System.out.println("Stalemate:"+b_child.isStalemate());
+			System.out.println("Checkmate:"+b_child.isCheckMate());
+			Node n_new =  new Node (b_child);
+			
+			n.addChild(n_new);
+			System.out.println(b_child.toString());
+			makeTree(n_new, vuelta);
+		}
+		*/
+		for (Move m:moves) {
+			Board b_child = b.clone();
+			b_child.makeMove(m);
+			Node n_new =  new Node (b_child);
+			n.addChild(n_new);
+			System.out.println("Movimiento: "+moves[0].toString());
+			System.out.println("Stalemate:"+b_child.isStalemate());
+			System.out.println("Checkmate:"+b_child.isCheckMate());
+			System.out.println(b_child.toString());
+			if(b_child.isCheckMate()){
+				System.out.println("Gano por jaquemate!");
+				return 1;
+			}
+			if(b_child.isStalemate()){
+				System.out.println("Ocurrio un empate :o !");
+				return 2;
+			}
+			if(vuelta<4){
+				int value = makeTree(n_new, vuelta);
+				if(value!=0){
+					return value;
+				}
+				
+			}
+		}
+		return 0;
+	}
 
 }
