f26a9ba54891ec3a667b86cd03cf746861ca4875
buster092 <buster092@gmail.com>
2011-12-14 22:41:26 +0000

    comments on dfs height

--- src/chess/DFS_HEIGHT.java
+++ src/chess/DFS_HEIGHT.java
@@ -6,7 +6,13 @@ import util.Node;
 
 public class DFS_HEIGHT {
 	private static int numnodes=0;
-	
+
+	/**
+	 * Performs an IDS search using DFS modified
+	 * @param b Board that represents the starting node for searching
+	 * @param max_height the maximum level to look up
+	 * @return the number of nodes that were looked up before to find the solution
+	 */
 	public static int IDS(Board b, int max_height){
 		for (int i = 0; i < max_height; i++) {
 			Move[] moves=b.getValidMoves();
@@ -23,6 +29,12 @@ public class DFS_HEIGHT {
 		}
 		return numnodes;
 	}
+	/**
+	 * Performs a DFS search restricted by a top of deeping (height)
+	 * @param b The current board (node)
+	 * @param height the deepest level to look up
+	 * @return 10 if a solution is found
+	 */
 	public static int DFS_HEIGHT(Board b, int height){
 		if(b.isCheckMate()){
 			if(b.turn== Board.TURNWHITE)
@@ -40,7 +52,7 @@ public class DFS_HEIGHT {
 			numnodes++;
 			int x = DFS_HEIGHT(b_child, height-1);
 			if(x==10){
-				System.out.println("Move Black:"+m);
+				//System.out.println("Move Black:"+m);
 				return x;
 			}	
 		}
