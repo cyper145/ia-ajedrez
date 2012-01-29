0b1505962c7c0ede006fea607e097353fc79f876
buster092@gmail.com <buster092@gmail.com@a617f9d4-fef7-f806-36dd-a0c89b619c63>
2012-01-29 01:09:35 +0000

    update entrenamiento, done

--- trunk/src/Server.java
+++ trunk/src/Server.java
@@ -144,11 +144,12 @@ public class Server {
     		}
 	}
 	
-	public void runTournament(IDSAgent[] agents){
+	public IDSAgent[] runTournament(IDSAgent[] agents){
 		
-		if(TORNEOS>0){
+//		if(TORNEOS>0){
 			/** reiniciar marcadores wins, draws, losses **/
 			for (int i = 0; i < agents.length; i++) {
+				System.out.println(agents[i].name);
 				agents[i].wins 		= 0;
 				agents[i].losses 	= 0;
 				agents[i].draws 	= 0;
@@ -167,37 +168,38 @@ public class Server {
 				
 			Sort.insertionSort(agents);	
 			
-			System.out.println("== Results ==");
-			for (int i = 0; i < agents.length; i++) {
-				System.out.println(agents[i].name+" wins:"+agents[i].wins + " Draws:" +agents[i].draws +" Losses:"+agents[i].losses+" "+Arrays.toString(agents[i].utility.values));
-			}
-			TORNEOS--;	
-			
-			/** Creacion de agentes mutados 2°(n/4) y nuevos agentes n/2 **/
-			if(TORNEOS>0){
-				for (int i = 0; i < agents.length/2; i++) {
-					/**
-					 * HACER LA MEZCLA DE CROMOSOMAS Y CREAR LOS NUEVOS AGENTES 
-					 * DICHOS AGENTES DEBEN REMPLAZAR LOS 2° n/4 PEORES, SIENDO LOS 
-					 * 1° N/4 LOS GANADORES ORIGINALES 
-					 */
-				}				
-				
-				for (int i = agents.length/2; i < agents.length; i++) {
-					Random r = new Random();
-					IDSAgent randomAgent=new IDSAgent(IDSAgent.MINIMAX,"moho"+(i+agents.length/2));
-					double g1 = r.nextInt(801)+100;
-					double g2 = r.nextInt(801)+100;
-					double g3 = r.nextInt(801)+100;
-					double g4 = r.nextInt(801)+100;
-					double g5 = r.nextInt(801)+100;
-					randomAgent.utility = new MaterialValue(new double[]{g1/100.0, g2/100.0, g3/100.0, g4/100.0, g5/100.0});
-					agents[i] = randomAgent;
-				}
-			}
-			
-			runTournament(agents);
-		}			
+			return agents;
+//			System.out.println("== Results ==");
+//			for (int i = 0; i < agents.length; i++) {
+//				System.out.println(agents[i].name+" wins:"+agents[i].wins + " Draws:" +agents[i].draws +" Losses:"+agents[i].losses+" "+Arrays.toString(agents[i].utility.values));
+//			}
+//			TORNEOS--;	
+//			
+//			/** Creacion de agentes mutados 2°(n/4) y nuevos agentes n/2 **/
+//			if(TORNEOS>0){
+//				for (int i = 0; i < agents.length/2; i++) {
+//					/**
+//					 * HACER LA MEZCLA DE CROMOSOMAS Y CREAR LOS NUEVOS AGENTES 
+//					 * DICHOS AGENTES DEBEN REMPLAZAR LOS 2° n/4 PEORES, SIENDO LOS 
+//					 * 1° N/4 LOS GANADORES ORIGINALES 
+//					 */
+//				}				
+//				
+//				for (int i = agents.length/2; i < agents.length; i++) {
+//					Random r = new Random();
+//					IDSAgent randomAgent=new IDSAgent(IDSAgent.MINIMAX,"moho"+(i+agents.length/2));
+//					double g1 = r.nextInt(801)+100;
+//					double g2 = r.nextInt(801)+100;
+//					double g3 = r.nextInt(801)+100;
+//					double g4 = r.nextInt(801)+100;
+//					double g5 = r.nextInt(801)+100;
+//					randomAgent.utility = new MaterialValue(new double[]{g1/100.0, g2/100.0, g3/100.0, g4/100.0, g5/100.0});
+//					agents[i] = randomAgent;
+//				}
+//			}
+//			
+//			runTournament(agents);
+//		}			
 	}
 	
 	
@@ -219,7 +221,7 @@ public class Server {
 				move=agents[currentagent].getBestMove(current_board, IDSAgent.MINIMAX);
 				if(DEBUG){
 					//System.out.println(current_board);
-					System.out.println("Move: "+move);
+					//System.out.println("Move: "+move);
 				}
 				
 				movimientos.add(move);
@@ -235,7 +237,7 @@ public class Server {
 				else current_board.makeMove(move);
 			//	System.out.println(b);
 			} else if (current_board.isStalemate()) {
-				System.out.println(current_board);
+				//System.out.println(current_board);
 				System.out.println("Stalemate");
 				winner=NOBODY;
 				loser=NOBODY;
@@ -244,7 +246,7 @@ public class Server {
 				draw=true;
 				gameover=true;
 			} else if (current_board.isCheckMate()) {
-				System.out.println(current_board);
+				//System.out.println(current_board);
 				System.out.println("Checkmate");
 				System.out.println(agents[nextAgent()].name + " has won!");
 				agents[nextAgent()].addWin();
@@ -263,44 +265,147 @@ public class Server {
 	 * TORNEO		= Num. de campeanatos, por cada uno se agregan nuevos participantes (n/4 ganadores, n/4 mezclas y n/2 nuevos agentes)
 	 */
 	public static void main(String[] args){
-		int num_agents = 4;
-		IDSAgent a1=new IDSAgent(IDSAgent.MINIMAX,"moho");
-		a1.utility = new MaterialValue(new double[]{0.6561000,2.1346,3.201,2.857,8.508});
-		IDSAgent a2=new IDSAgent(IDSAgent.MINIMAX,"moho2");
-		a2.utility = new MaterialValue(new double[]{1.6561000,2.7346,3.201,5.857,3.508});
-		
-		Server s=new Server(num_agents);
-		s.TORNEOS = 4;
-		
-		
-		/***
-		 * Genera 100 agentes con valores randomizados en sus cromosomas
-		 */
-		for (int i = 0; i < num_agents; i++) {
-			Random r = new Random();
-			IDSAgent randomAgent=new IDSAgent(IDSAgent.MINIMAX,"moho"+i);
-			double g1 = r.nextInt(801)+100;
-			double g2 = r.nextInt(801)+100;
-			double g3 = r.nextInt(801)+100;
-			double g4 = r.nextInt(801)+100;
-			double g5 = r.nextInt(801)+100;
-			randomAgent.utility = new MaterialValue(new double[]{g1/100.0, g2/100.0, g3/100.0, g4/100.0, g5/100.0});
-			agents_ids[i] = randomAgent;
+		int num_agents = 100;
+//		IDSAgent a1=new IDSAgent(IDSAgent.MINIMAX,"moho");
+//		a1.utility = new MaterialValue(new double[]{0.6561000,2.1346,3.201,2.857,8.508});
+//		IDSAgent a2=new IDSAgent(IDSAgent.MINIMAX,"moho2");
+//		a2.utility = new MaterialValue(new double[]{1.6561000,2.7346,3.201,5.857,3.508});
+		while(true){
+			Server s=new Server(num_agents);
+			s.TORNEOS = 4;
+			
+			String gen[];
+			double params[] = null;
+			//lee los top 10 de los genes del archivo top10gens.txt
+			try {
+				BufferedReader input =   new BufferedReader(new FileReader("top10gens.txt"));
+				for (int i = 0; i < 10; i++) {	
+					IDSAgent topAgent=new IDSAgent(IDSAgent.MINIMAX,"moho"+i);					
+					
+					String line=input.readLine();
+					System.out.println("linea leida: "+line);
+					gen = line.split(" ");
+					params = new double[gen.length];
+					for (int j = 0; j < gen.length; j++) {
+						params[j] = Double.parseDouble(gen[j]);
+					}
+					topAgent.utility = new MaterialValue(params);
+					agents_ids[num_agents-1-i] = topAgent;
+				}
+			}catch (Exception e) {
+				System.out.println("Error al leer el archivo: "+e.getMessage());
+			}
+			
+			/***
+			 * Genera los 90 agentes restantes con valores randomizados en sus cromosomas
+			 * (suponemos que num_agents = 100)
+			 */
+			for (int i = 0; i < num_agents-10; i++) {
+				Random r = new Random();
+				IDSAgent randomAgent=new IDSAgent(IDSAgent.MINIMAX,"moho"+i);
+				double g1 = r.nextInt(801)+100;
+				double g2 = r.nextInt(801)+100;
+				double g3 = r.nextInt(801)+100;
+				double g4 = r.nextInt(801)+100;
+				double g5 = r.nextInt(801)+100;
+				randomAgent.utility = new MaterialValue(new double[]{g1/100.0, g2/100.0, g3/100.0, g4/100.0, g5/100.0});
+				agents_ids[i] = randomAgent;
+			}
+			
+			init_board = s.loadBoard();
+			current_board = init_board.clone();
+			//hace el torneo con los 100 agentes
+			IDSAgent[] agents_ids_aux = agents_ids;
+			//IDSAgent[] agents_ids_aux = s.runTournament(agents_ids);
+			
+			//guarda al mejor del torneo
+			writeBestAgent(agents_ids_aux[0]);
+			System.out.println("mejor agente guardado");
+			IDSAgent[] mutados = new IDSAgent[10];
+			
+			//genera 10 nuevos agentes a partir de los 50 primeros
+			for (int i = 0; i < 10; i++) {
+				Random r = new Random();
+				IDSAgent [] seleccionados = new IDSAgent[5];
+				int pos = 0;
+				//selecciona a los 5 agentes de manera aleatoria
+				while(pos <5){
+					int indice = r.nextInt(50);
+					if(agents_ids_aux[indice]!=null){
+						seleccionados[pos] = agents_ids_aux[indice];
+						pos++;
+						agents_ids_aux[indice]=null;
+					}
+				}
+
+				System.out.println("agentes generados");
+				IDSAgent newAgent=new IDSAgent(IDSAgent.MINIMAX,"mohoV2_"+i);
+				pos = 0;
+				r = new Random();
+				double [] newValues = new double[5];
+				//a partir del los seleccionados, elije los cromosomas para el nuevo agente
+				while(pos <5){
+					int indice = r.nextInt(5);
+					if(seleccionados[indice]!=null){
+						newValues[pos] = seleccionados[indice].utility.values[pos];
+						pos++;
+						seleccionados[indice]=null;
+					}
+				}
+				newAgent.utility = new MaterialValue(newValues);
+				mutados[i] = newAgent;	
+				
+			}
+			System.out.println("guardando top 10 agentes");
+			try{
+				FileWriter fstream = new FileWriter("top10gens.txt");
+				BufferedWriter out = new BufferedWriter(fstream);
+				String st = "";
+				for (int i = 0; i < mutados.length; i++) {
+					for (int j = 0; j < mutados[i].utility.values.length; j++) {
+						st = st+mutados[i].utility.values[j]+" ";						
+					}
+					st = st.substring(0, st.length()-1);
+					st = st+"\n";
+				}
+				out.write(st);
+				out.close();
+				}catch (Exception e){
+				System.err.println("Error: " + e.getMessage());
+			}
+			
+			System.out.println("top 10 agentes guardados");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			/***
+			 * Al finalizar el torneo se tiene un ranking de todos
+			 * De todos ellos se selecciona a los 50 mejores
+			 * Se crean 10 nuevos agentes en donde cada uno de los cromosomas de estos agentes corresponde a un valor de un ganador,
+			 * asi 1 nuevo agente es generado a partir de 5 ganadores (elejidos de manera aleatoria)
+			 * Ej: cromosomaNuevoAgente = {cromGanador1[2], cromGanador2[5], cromGanador3[1], cromGanador4[1], cromGanador5[3] }
+			 * Estos 10 nuevos agentes deben jugar nuevamente con una poblacion de 90 nuevos agentes randomizados, and so on...
+			 */
 		}
-		
-		init_board = s.loadBoard();
-		current_board = init_board.clone();
-		
-		s.runTournament(agents_ids);
-		
-		/***
-		 * Al finalizar el torneo se tiene un ranking de todos
-		 * De todos ellos se selecciona a los 50 mejores
-		 * Se crean 10 nuevos agentes en donde cada uno de los cromosomas de estos agentes corresponde a un valor de un ganador,
-		 * asi 1 nuevo agente es generado a partir de 5 ganadores (elejidos de manera aleatoria)
-		 * Ej: cromosomaNuevoAgente = {cromGanador1[2], cromGanador2[5], cromGanador3[1], cromGanador4[1], cromGanador5[3] }
-		 * Estos 10 nuevos agentes deben jugar nuevamente con una poblacion de 90 nuevos agentes randomizados, and so on...
-		 */
 	}
-	
+	public static void writeBestAgent(IDSAgent agente) {
+		// TODO Auto-generated method stub
+		try{
+			FileWriter fstream = new FileWriter("bestAgent.txt");
+			BufferedWriter out = new BufferedWriter(fstream);
+			String s = "";
+			for (int i = 0; i < agente.utility.values.length; i++) {
+				s = s+agente.utility.values[i]+" ";
+			}
+			s = s.substring(0, s.length()-1);
+			out.write(s);
+			out.close();
+			}catch (Exception e){
+			System.err.println("Error: " + e.getMessage());
+			}
+		}
+
 }
